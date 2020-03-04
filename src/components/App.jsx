779675285ccc7fba2faf371c666c7a2065a51859
Viacheslav <deniskosv@gmail.com>
2020-03-04 17:24:49 +0200

    created project

---
+++ src/components/App.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import {moviesData} from "../moviesData";
+import MovieItem from "./MovieItem";
+
+// UI = fn(state, props) App = new React.Component()
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData,
+      moviesWillWatch: []
+    };
+  }
+
+  deleteMovie = movie => {
+    const updateMovies = this
+      .state
+      .movies
+      .filter(item => item.id !== movie.id);
+
+    // this.state.movies = updateMovies;
+    this.setState({movies: updateMovies});
+  };
+
+  addMovie = movie => {
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    updateMoviesWillWatch.push(movie);
+
+    this.setState({moviesWillWatch: updateMoviesWillWatch});
+  };
+
+  removeMovie = movie => {
+    const updateMoviesWillWatch = this
+      .state
+      .moviesWillWatch
+      .filter(item => item.id !== movie.id);
+
+    this.setState({moviesWillWatch: updateMoviesWillWatch});
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-9">
+            <div className="row">
+              {this
+                .state
+                .movies
+                .map(movie => {
+                  return (
+                    <div className="col-6 mb-4" key={movie.id}>
+                      <MovieItem
+                        data={movie}
+                        deleteMovie={this.deleteMovie}
+                        addMovie={this.addMovie}
+                        removeMovie={this.removeMovie}/>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+          <div className="col-3">
+            <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
+            <ul className="list-group">
+              {this
+                .state
+                .moviesWillWatch
+                .map(movie => (
+                  <li key={movie.id} className="list-group-item">
+                    <div className="d-flex justify-content-between">
+                      <span>{movie.title}</span>
+                      <span>{movie.vote_average}</span>
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MovieItem.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+class MovieItem extends React.Component {
+  state = {
+    willWatch: false
+  };
+
+  render() {
+    const {
+      data,
+      deleteMovie,
+      addMovie,
+      removeMovie
+    } = this.props;
+    // props.data = {};
+    return (
+      <div className="card text-white bg-dark mb-3">
+        <img
+          className="card-img-top"
+          src={`https://image.tmdb.org/t/p/w500${data.backdrop_path ||
+            data.poster_path}`}
+          alt=""
+        />
+        <div className="card-body">
+          <h6 className="card-title">{data.title}</h6>
+          <h6 className="mb-3">Rating: {data.vote_average}</h6>
+          <div className="d-flex justify-content-between align-items-center">
+            {this.state.willWatch ? (
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => {
+                  this.setState({
+                    willWatch: false
+                  });
+                  removeMovie(data);
+                }}
+              >
+                Remove from Favorites
+              </button>
+            ) : (
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  onClick={() => {
+                    this.setState({
+                      willWatch: true
+                    });
+                    addMovie(data);
+                  }}
+                >
+                  Add to Favorites
+              </button>
+              )}
+          
+          <button
+            type="button"
+            class="btn btn-danger"
+            onClick={() => {
+              deleteMovie(data);
+            }}
+          >
+            Delete movie
+          </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
+
+{/* <div className="card-body">
+          <h6 className="card-title">{data.title}</h6>
+          <h6 className="mb-4">Rating: {data.vote_average}</h6>
+          <div className="d-flex justify-content-between align-items-center">
+            {this.state.willWatch
+              ? <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => {
+                  this.setState({willWatch: false});
+                  removeMovie(data);
+                }}>Remove from Favorites</button>
+              : <button
+                type="button"
+                className="btn btn-success"
+                onClick={() => {
+                this.setState({willWatch: true});
+                addMovie(data);
+              }}>
+                Add to Favorites
+              </button>}
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={deleteMovie(data)}>
+              Delete movie
+            </button>
+          </div>
+        </div> */}
